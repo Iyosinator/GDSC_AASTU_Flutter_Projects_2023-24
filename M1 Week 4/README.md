977ddd7cadb36e6da706e573638c88bbf3799d84
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-02-29 14:50:18 +0300

    todolist

--- M1 Week 4/README.md
+++
@@ -1,6 +0,0 @@
-# todo_list
-
-# Implementing this Screen
-## https://www.figma.com/file/oMCL0SuMcmKi9YgcDTwMLR/Untitled?type=design&node-id=0-1&mode=design&t=oDs4x8IpsYBGtvbA-0
-# todo_list
-
---
+++ M1 Week4/todolist/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(237, 202, 11, 11),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 90,
+            ),
+            Image.asset('assets/stickman.png'),
+            const Spacer(),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/todolist');
+              },
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 60),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  )),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
